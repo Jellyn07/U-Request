ab555fb5c45655f6ab0add9e4efd822993612c0f
J. <jonagujol@gmail.com>
2025-10-24 01:32:56 +0800

    DB Fixed - u_request  (db name)
    Ang sql naas GC paki import nlang ko, then if mag export mog db check custom.

--- app/core/BaseModel.php
+++ app/core/BaseModel.php
@@ -5,7 +5,7 @@ class BaseModel {
     public $db;
 
     public function __construct() {
-        $this->db = new mysqli("localhost", "root", "", "urequest_db");
+        $this->db = new mysqli("localhost", "root", "", "u_request");
         if ($this->db->connect_errno) {
             die("DB Connection failed: " . $this->db->connect_error);
         }
